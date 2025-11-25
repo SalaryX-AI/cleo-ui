2f4c73a3c5f2be127916e857684deb66b540f0c9
MuammadMuaaz9 <maazjanjua9@gmail.com>
2025-11-26 01:46:26 +0500

    Version 1.0

--- config.js
+++
@@ -1,5 +0,0 @@
-window.apiBaseUrl = "http://localhost:8000"; 
-window.wsBaseUrl  = "ws://localhost:8000";
-
-// console.log("API Base URL:", window.apiBaseUrl);
-// console.log("WS Base URL:", window.wsBaseUrl);
