c02e2e35039f99bb0514acc6bd8bcfdaaf0f3c5a
hhamzakhann <hamzabahramand@hotmail.com>
2025-11-28 21:58:47 +0500

    Create config.js

---
+++ client-websites/big_chicken_frontend/config.js
@@ -0,0 +1,5 @@
+window.apiBaseUrl = "http://13.58.213.231"; 
+window.wsBaseUrl  = "ws://13.58.213.231";
+
+// console.log("API Base URL:", window.apiBaseUrl);
+// console.log("WS Base URL:", window.wsBaseUrl);
