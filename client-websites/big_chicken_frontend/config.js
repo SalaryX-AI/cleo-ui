e4a26ce5c7bf2bcb5f39e2f9c7437ca623d58188
Muhammad Muaaz <75490196+MuhammadMuaaz9@users.noreply.github.com>
2025-12-04 18:00:01 +0500

    Update API and WS base URLs to production

--- client-websites/big_chicken_frontend/config.js
+++ client-websites/big_chicken_frontend/config.js
@@ -1,5 +1,5 @@
-window.apiBaseUrl = "http://13.58.213.231"; 
-window.wsBaseUrl  = "ws://13.58.213.231";
+window.apiBaseUrl = "https://scanandhire.com";
+window.wsBaseUrl  = "ws://scanandhire.com";
 
 // console.log("API Base URL:", window.apiBaseUrl);
 // console.log("WS Base URL:", window.wsBaseUrl);
