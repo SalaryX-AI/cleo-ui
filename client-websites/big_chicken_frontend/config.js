665ab5a884e4d0c0eb01db198d743c1fe50e9c34
Muhammad Muaaz <75490196+MuhammadMuaaz9@users.noreply.github.com>
2025-12-05 00:12:54 +0500

    Update WebSocket URL to use secure connection

--- client-websites/big_chicken_frontend/config.js
+++ client-websites/big_chicken_frontend/config.js
@@ -1,5 +1,5 @@
 window.apiBaseUrl = "https://scanandhire.com";
-window.wsBaseUrl  = "ws://scanandhire.com";
+window.wsBaseUrl  = "wss://scanandhire.com";
 
 // console.log("API Base URL:", window.apiBaseUrl);
 // console.log("WS Base URL:", window.wsBaseUrl);
