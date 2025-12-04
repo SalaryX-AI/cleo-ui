352115d85a27805f7dbe5e1cb1cac73c714db06b
Muhammad Muaaz <75490196+MuhammadMuaaz9@users.noreply.github.com>
2025-12-05 00:15:32 +0500

    config.js added

---
+++ client-websites/McDonald_frontend/config.js
@@ -0,0 +1,5 @@
+window.apiBaseUrl = "https://scanandhire.com";
+window.wsBaseUrl  = "wss://scanandhire.com";
+
+// console.log("API Base URL:", window.apiBaseUrl);
+// console.log("WS Base URL:", window.wsBaseUrl);
