bcfc620c6cc2012c61e0197665ea1f584892ebcb
Muhammad Muaaz <75490196+MuhammadMuaaz9@users.noreply.github.com>
2026-01-18 22:24:44 +0500

    Update API and WS base URLs to production

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
-window.apiBaseUrl = "http://localhost:8000"; 
-window.wsBaseUrl  = "ws://localhost:8000";
+window.apiBaseUrl = "https://scanandhire.com";
+window.wsBaseUrl  = "wss://scanandhire.com";
 
 // console.log("API Base URL:", window.apiBaseUrl);
 // console.log("WS Base URL:", window.wsBaseUrl);
